f1459c0dd57edd99b2e09ccf3ba578292dc2a06a
seungin <tmddls91@inha.edu>
2025-01-02 01:23:17 +0900

    JavaScript - handling events

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -21,11 +21,40 @@ function createHTMLString(item) {
     `;
 }
 
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+  
+  if (key == null || value == null) {
+    return;
+  }
+
+  displayItems(items.filter(item => item[key] === value));
+}
+
+function updateItems(items, key, value) {
+  items.forEach(item => {
+    if (item.dataset[key] === value) {
+      item.classList.remove('indisible');
+    } else {
+      item.classList.add('imvisible');
+    }
+  });
+}
+
+function setEventListners(items) {
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', event => onButtonClick(event,items));
+}
+
 // main
 loadItems() 
   .then(items => {
     console.log(items);
     displayItems(items);
-    //setEventListners(items)
+    setEventListners(items)
   })
   .catch(console.log)
