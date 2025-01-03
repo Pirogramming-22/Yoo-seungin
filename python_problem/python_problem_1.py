c446be30ee5e6f750deded8b1604fd13a5701a3f
seungin <tmddls91@inha.edu>
2025-01-04 00:49:28 +0900

    8단계 커밋 후 최종 단계 9단계로 코드 돌려놓기

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,29 +1,33 @@
+import random as r
+
 def brGame(player):
     global num
-    while True:
-        try:
-            user_input = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-            if user_input not in [1, 2, 3]:
-                print("1, 2, 3 중 하나를 입력하세요")
-                continue
-            break
-        except ValueError:
-            print("정수를 입력하세요")
+    if player == "computer":
+        user_input = r.randint(1,3)
+    else:
+        while True:
+            try:
+                user_input = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+                if user_input not in [1, 2, 3]:
+                    print("1, 2, 3 중 하나를 입력하세요")
+                    continue
+                break
+            except ValueError:
+                print("정수를 입력하세요")
 
     for i in range(1, user_input+1):
         num += 1
         print(f"{player} : {num}")
         if num >= 31:
-            print("=" * 58)
-            print(f"player{'A' if player == 'playerB' else 'B'} win!")
+            print("=======================================================================")
+            print(f"{'player' if player == 'computer' else 'computer'} win!")
             return True
     return False
 
 num = 0
 
 while num < 31:
-    if brGame("playerA"):
+    if brGame("computer"):
         break
-    if brGame("playerB"):
-        break 
-
+    if brGame("player"):
+        break
