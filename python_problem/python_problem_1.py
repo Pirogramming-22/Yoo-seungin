ad1938f555f5b8ca3a227c95932578fe7452f8c1
seungin <tmddls91@inha.edu>
2025-01-04 00:02:10 +0900

    python_problem_1: 4단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -7,7 +7,11 @@ while True:
         if user_input not in [1, 2, 3]:
             print("1, 2, 3 중 하나를 입력하세요")
             continue
-
         break
     except ValueError:
         print("정수를 입력하세요")
+
+
+for i in range(1, user_input+1):
+    num += 1
+    print(f"playerA : {num}")
