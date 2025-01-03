94a9fda9ac445b7b8f44a0d370aa46f84bedaf2e
seungin <tmddls91@inha.edu>
2025-01-04 00:45:18 +0900

    python_problem_1: 8단계 (커밋을 빼먹고 안해서 8단계로 코드 수정 후 커밋)

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,34 +1,29 @@
-import random as r
-
 def brGame(player):
     global num
-    if player == "computer":
-        user_input = r.randint(1,3)
-    else:
-        while True:
-            try:
-                user_input = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-                if user_input not in [1, 2, 3]:
-                    print("1, 2, 3 중 하나를 입력하세요")
-                    continue
-                break
-            except ValueError:
-                print("정수를 입력하세요")
+    while True:
+        try:
+            user_input = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+            if user_input not in [1, 2, 3]:
+                print("1, 2, 3 중 하나를 입력하세요")
+                continue
+            break
+        except ValueError:
+            print("정수를 입력하세요")
 
     for i in range(1, user_input+1):
         num += 1
         print(f"{player} : {num}")
         if num >= 31:
-            print("=======================================================================")
-            print(f"{'player' if player == 'computer' else 'computer'} win!")
+            print("=" * 58)
+            print(f"player{'A' if player == 'playerB' else 'B'} win!")
             return True
     return False
 
 num = 0
 
 while num < 31:
-    if brGame("computer"):
+    if brGame("playerA"):
         break
-    if brGame("player"):
-        break
+    if brGame("playerB"):
+        break 
 
