165e9800eb6d7a239e6e131019035cdefab036b5
seungin <tmddls91@inha.edu>
2025-01-15 05:48:01 +0900

    개발툴 등록, 개발툴 관리 기능 일부 구현

--- SWIDEA_SITE/ideas/forms.py
+++ SWIDEA_SITE/ideas/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Idea
+from .models import DevTool
 
 class IdeaForm(forms.ModelForm):
     class Meta:
@@ -11,4 +12,24 @@ class IdeaForm(forms.ModelForm):
             'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': '아이디어 내용을 입력하세요'}),
             'interest': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': '관심도를 입력하세요'}),
             'devtool': forms.Select(attrs={'class': 'form-control'})
+        }
+        
+        
+class DevToolForm(forms.ModelForm):
+    class Meta:
+        model = DevTool
+        fields = ['name', 'kind', 'content']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': '개발툴 이름을 입력하세요'
+            }),
+            'kind': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': '개발툴 종류를 입력하세요'
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': '개발툴 설명을 입력하세요'
+            }),
         }
--- SWIDEA_SITE/ideas/views.py
+++ SWIDEA_SITE/ideas/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
-from .models import Idea
-from .forms import IdeaForm
+from django.db.models import Count
+from .models import Idea, DevTool
+from .forms import IdeaForm, DevToolForm
 
 def idea_list(request):
+    sort_option = request.GET.get('sort', 'latest')
+    
+    if sort_option == 'stars':
+        ideas = Idea.objects.annotate(star_count=Count('stars')).order_by('-star_count')
+    elif sort_option == 'name':
+        ideas = Idea.objects.order_by('title')
+    elif sort_option == 'created':
+        ideas = Idea.objects.order_by('created_at')
+    else:  
+        ideas = Idea.objects.order_by('-created_at')
     
     paginator = Paginator(ideas, 4)
     page = request.GET.get('page')
@@ -11,8 +22,9 @@ def idea_list(request):
     
     return render(request, 'ideas/idea_list.html', {
         'ideas': ideas,
+        'sort_option': sort_option
     })
-    
+
 def idea_create(request):
     if request.method == 'POST':
         form = IdeaForm(request.POST, request.FILES)
@@ -24,13 +36,21 @@ def idea_create(request):
     else:
         form = IdeaForm()
     return render(request, 'ideas/idea_form.html', {'form': form})
-  
-def devtool_create(request):
-    return render(request, 'ideas/devtool_form.html', {'form': form})
 
-def devtool_list(request):
-    return render(request, 'ideas/devtool_list.html', {'devtools': devtools})
-  
 def idea_detail(request, pk):
     idea = get_object_or_404(Idea, pk=pk)
     return render(request, 'ideas/idea_detail.html', {'idea': idea})
+
+def devtool_list(request):
+    devtools = DevTool.objects.all().order_by('name')
+    return render(request, 'ideas/devtool_list.html', {'devtools': devtools})
+
+def devtool_create(request):
+    if request.method == 'POST':
+        form = DevToolForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('ideas:devtool_list')
+    else:
+        form = DevToolForm()
+    return render(request, 'ideas/devtool_form.html', {'form': form})
